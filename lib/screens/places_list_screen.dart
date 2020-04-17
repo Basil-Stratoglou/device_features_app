92de72bdca72b0973e0a37fe04aebc27bc949e25
Vasilis <billdelta62@gmail.com>
2020-04-17 17:03:23 +0200

    camera usage and saving images on device

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
+import 'package:device_features_app/providers/places_provider.dart';
 import 'package:device_features_app/screens/add_place_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
@@ -17,7 +19,25 @@ class PlacesListScreen extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Consumer<PlacesProvider>(
+          child: Center(
+            child: Text('You have no places yet. Add some first!'),
+          ),
+          builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+              ? ch
+              : ListView.builder(
+                  itemCount: greatPlaces.items.length,
+                  itemBuilder: (ctx, i) => ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: FileImage(
+                        greatPlaces.items[i].image,
+                      ),
+                    ),
+                    title: Text(greatPlaces.items[i].title),
+                    onTap: () {},
+                  ),
+                ),
+        ),
       ),
     );
   }
